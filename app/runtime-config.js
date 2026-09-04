3483fd070d72f3f8efe17746f40dcdc18b71e5ae
vbgATgh <vbginbox@outlook.com>
2026-09-04 21:56:55 +0200

    chore(runtime): align v8.7.15

--- app/runtime-config.js
+++ app/runtime-config.js
@@ -13,7 +13,7 @@ try{
   }
 }catch{}
 window.HPOS_RUNTIME=Object.freeze({
-  version:'8.7.14',
+  version:'8.7.15',
   integration:Object.freeze({
     enabled:true,
     mode:'SUPABASE_PARQET_QUOTES_SEARCH',
