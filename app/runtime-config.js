4737c5ff90e89dd3aeafec2af0fa8d1670fba6f2
vbgATgh <vbginbox@outlook.com>
2026-09-06 18:27:39 +0200

    chore: bump runtime to v8.7.33

--- app/runtime-config.js
+++ app/runtime-config.js
@@ -13,7 +13,7 @@ try{
   }
 }catch{}
 window.HPOS_RUNTIME=Object.freeze({
-  version:'8.7.32',
+  version:'8.7.33',
   integration:Object.freeze({
     enabled:true,
     mode:'SUPABASE_PARQET_QUOTES_SEARCH',
